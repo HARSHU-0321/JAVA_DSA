5213791c06873636fd05033cd03cb492f1564e68
HARSHU-0321 <marrihasrhitha3@gmail.com>
2026-05-23 09:46:14 +0530

    added day 5

---
+++ src/day5/Fibonacci.java
@@ -0,0 +1,25 @@
+package day5;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+     public static int fibo(int n){
+        
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        
+        //recursive call
+        return fibo(n-1)+fibo(n-2);
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        for(int i=0; i<=0;i++){
+        System.out.println(fibo(i));
+        }
+
+
+    } 
+}
+
